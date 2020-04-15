6e4a10f444e25cf612cd4602cecfcaca4af3ecfa
TosHiki <ichi.toshi217@gmail.com>
2020-04-15 15:52:54 +0900

    Initial Commit

---
+++ Tabbed_app_test/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Tabbed_app_test
+//
+//  Created by Toshiki Ichibangase on 2020/04/15.
+//  Copyright © 2020 Toshiki Ichibangase. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
