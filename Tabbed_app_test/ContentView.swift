e2cd37c4092ee82bcd66b052232dd596f7b96c48
TosHiki <ichi.toshi217@gmail.com>
2020-05-22 16:53:28 +0900

    Make Switch of Darkmode applying whole app.

--- Tabbed_app_test/ContentView.swift
+++ Tabbed_app_test/ContentView.swift
@@ -8,47 +8,24 @@
 
 import SwiftUI
 
+class CustomEnvironment : ObservableObject {
+    @Published var colorScheme: ColorScheme = .dark
+}
+
 struct ContentView: View {
     @State private var selection = 0
     @State private var colorScheme: ColorScheme = .dark
- 
+    //@Environment(\.colorScheme) var colorScheme
+    
+    @ObservedObject var env = CustomEnvironment()
+    
     var body: some View {
-        TabView(selection: $selection){
-            IndicatorView()
-                .tabItem {
-                    VStack {
-                        Image(systemName: "house.fill")
-                        Text("Indicator")
-                    }
-                }
-                .tag(0)
-            LineChartView()
-                .environment(\.colorScheme, colorScheme)
-                .tabItem {
-                    VStack {
-                        Image(systemName: "square.stack.3d.down.right")
-                        Text("Line Chart")
-                    }
-                }
-                .tag(1)
-            
-            Text("Third View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image(systemName: "photo")
-                            .scaledToFit()
-                            .overlay(Color.primary)
-                        Text("Third")
-                    }
-                }
-                .tag(2)
-        }
+        CustomTabView(env: env).environment(\.colorScheme, env.colorScheme)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(env:  CustomEnvironment()).environment(\.colorScheme, .dark)
     }
 }
---
+++ Tabbed_app_test/CustomTabView.swift
@@ -0,0 +1,78 @@
+//
+//  CustomTabView.swift
+//  Tabbed_app_test
+//
+//  Created by Toshiki Ichibangase on 2020/05/22.
+//  Copyright © 2020 Toshiki Ichibangase. All rights reserved.
+//
+
+import SwiftUI
+
+struct CustomTabView: View {
+    @State private var selection = 0
+    //@State private var colorScheme: ColorScheme = .dark
+    @Environment(\.colorScheme) var colorScheme
+    
+    @ObservedObject var env: CustomEnvironment
+    
+    var body: some View {
+        GeometryReader { geo in
+            TabView(selection: self.$selection){
+                IndicatorView()
+                    .tabItem {
+                        VStack {
+                            Image(systemName: "house.fill")
+                            Text("Indicator")
+                        }
+                    }
+                    .tag(0)
+                VStack{
+                    LineChartView()
+                        .environment(\.colorScheme, self.env.colorScheme)
+                    
+                    Button(action: {
+                        if self.colorScheme == .dark {
+                            withAnimation {
+                                self.env.colorScheme = .light
+                            }
+                        } else {
+                            withAnimation {
+                                self.env.colorScheme = .dark
+                            }
+                        }
+                    }) {
+                        Text("Dark mode On/Off")
+                            .frame(width: geo.size.width, height: 50)
+                            //.background(self.colorScheme == .dark ? Color.black : Color.white)
+                    }
+                }.background(self.colorScheme == .dark ? Color.black : Color.white)
+                    .tabItem {
+                        VStack {
+                            Image(systemName: "square.stack.3d.down.right")
+                            Text("Line Chart")
+                        }
+                    }
+                    .tag(1)
+                
+                Text("Third View")
+                    .font(.title)
+                    .tabItem {
+                        VStack {
+                            Image(systemName: "photo")
+                                .scaledToFit()
+                                .overlay(Color.primary)
+                            Text("Third")
+                        }
+                    }
+                    .tag(2)
+            }
+        }
+    }
+}
+
+struct CustomTabView_Previews: PreviewProvider {
+    @ObservedObject var env = CustomEnvironment()
+    static var previews: some View {
+        CustomTabView(env: CustomEnvironment()).environment(\.colorScheme, .dark)
+    }
+}
